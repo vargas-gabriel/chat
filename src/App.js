5a2ff057dbed859ac7ac747602877dc05a975dab
Gabriel Vargas <vargasgabriel@protonmail.com>
2021-02-08 20:41:32 -0600

    set up appjs file with chatengine

--- src/App.js
+++ src/App.js
@@ -1,2 +1,8 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+
+const App = () => {
+	return <ChatEngine height=' 100vh' projectID='' userName='' userSecret='' />;
+};
+
+export default App;
