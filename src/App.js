77337b2898db3a6c9632618fdcc19a9ee7fb3cf5
Gabriel Vargas <vargasgabriel@protonmail.com>
2021-02-12 17:32:16 -0600

    added chatengine to appjs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 const App = () => {
-	return <ChatEngine height=' 100vh' projectID='' userName='' userSecret='' />;
+	return (
+		<ChatEngine
+			height=' 100vh'
+			projectID='placeholder'
+			userName=''
+			userSecret=''
+		/>
+	);
 };
 
 export default App;
